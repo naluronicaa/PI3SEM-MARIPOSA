4e3d9cd843081bb486219935bcea049c4d5dd1e0
naluronicaa <106675336+naluronicaa@users.noreply.github.com>
2023-05-12 08:03:25 -0300

    Atualização DBConexion.java
    Correção da conexão com o Banco de Dados

--- DBConexion.java
+++ DBConexion.java
@@ -1,9 +1,10 @@
 package com.mysql.conexion;
 
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
+import java.sql.ResultSet;
 
 public class DBConexion {
 
@@ -12,22 +13,46 @@ public class DBConexion {
     private static String SENHA = "*uaAOYF45dg1";
     private static String URL = "jdbc:mysql://localhost:3306/dbmariposa";
 
-    static {
+    public static Connection getConnection(){
         try {
             Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "Erro com o Driver" + e);
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        }catch (ClassNotFoundException | SQLException e){
+            throw new RuntimeException("Erro na conexão com o banco de dados: "+e);
         }
     }
-
-    public Connection getConnection() {
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(URL, USUARIO, SENHA);
-            JOptionPane.showMessageDialog(null, "Conexão Bem Sucedida");
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro com a Conexão" + e);
+    
+    public static void closeConnection(Connection con){
+        try{
+            if (con  != null){
+                con.close();
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dado+ " + e);
         }
-        return con;
     }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+        closeConnection(con);
+        try{
+            if(stmt != null){
+                stmt.close();
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dado+ " + e);
+        }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+        closeConnection(con, stmt);
+        try{
+            if (rs != null){
+                rs.close();
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dado+ " + e);
+        }
+    }
+    
+    
 }
