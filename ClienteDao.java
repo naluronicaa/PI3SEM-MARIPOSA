7e7da465a10b4c9bb8bd5194c22c8ceee78590c2
nana <106675336+naluronicaa@users.noreply.github.com>
2023-05-16 12:07:58 -0300

    Update ClienteDao.java

--- ClienteDao.java
+++ ClienteDao.java
@@ -1,7 +1,5 @@
-
 package com.mysql.conexion.ClienteDao;
 
-import VIEW.PaginaInicial;
 import com.classes.clientes.Cliente;
 import com.mysql.conexion.DBConexion;
 import java.sql.PreparedStatement;
@@ -24,51 +22,234 @@ public class ClienteDao {
             
             stmt.executeUpdate();
             
-            JOptionPane.showMessageDialog(null, "Cadastro feito com sucesso!");
+            posDados(c);
+            
             
         }catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Houve algum erro ao salvar seus dados" + e.toString());
         }finally{
             DBConexion.closeConnection(con, stmt);
         }
+        
     }
     
-    public void fazerLogin(Cliente c) throws SQLException {
+    public void posDados (Cliente c) {
+        Connection con = DBConexion.getConnection();
+        String sql = "Select id_usuario from usuarios where email=?";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+                
+        try{
+            stmt = con.prepareStatement(sql);
+            
+            stmt.setString(1, c.getEmail());
+            
+            rs = stmt.executeQuery();
+            
+            if (rs.next()){
+                int id = rs.getInt("id_usuario");
+                c.setId(id);
+            } else{
+                JOptionPane.showMessageDialog(null, "DEU MT RUIM :D");
+                c.setId(0);
+            }
+            
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Houve algum erro: "+ e);
+        } finally {
+            DBConexion.closeConnection(con, stmt, rs);
+        }
+    }
+    
+    public boolean fazerLogin(Cliente c) throws SQLException {
         Connection con = DBConexion.getConnection();
         String sql = "Select * from usuarios where email=? and senha=?";
         String sql2 = "Select id_usuario from usuarios where email=?";
+        boolean log = false;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
         try{
             
-            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt = con.prepareStatement(sql);
             
             stmt.setString(1, c.getEmail());
             stmt.setString(2, c.getSenha());
             
-            ResultSet rs = stmt.executeQuery();
+            rs = stmt.executeQuery();
             if (rs.next()){
-                JOptionPane.showMessageDialog(null, "Login jeito com sucesso!");
-                PreparedStatement sm = con.prepareStatement(sql2);
-                sm.setString(1, c.getEmail());
+                log = true;
+                stmt = con.prepareStatement(sql2);
+                stmt.setString(1, c.getEmail());
                 
-                ResultSet rs2 = sm.executeQuery();
-                if (rs2.next()){
-                    int id = rs2.getInt("id_usuario");
+                rs = stmt.executeQuery();
+                if (rs.next()){
+                    int id = rs.getInt("id_usuario");
                     c.setId(id);
-                    JOptionPane.showMessageDialog(null, "ID logado: " + c.getId());
                 } else{
                     JOptionPane.showMessageDialog(null, "deu ruim");
                 }
                 
-                
             }else{
-                JOptionPane.showMessageDialog(null, "Não existe nenhum cadastro com esses dados!");
+                log = false;
                 c.setId(0);
             }
         } catch (SQLException e){
             JOptionPane.showMessageDialog(null, "Houve algum erro ao fazer o Login" + e.toString());
         } finally{
-            DBConexion.closeConnection(con);
+            DBConexion.closeConnection(con, stmt, rs);
+        
+        }
+        
+        return log;    
+    }
+    
+    public void trazerDados(Cliente c) throws SQLException{
+        Connection con = DBConexion.getConnection();
+        String sql = "Select email, senha from usuarios where id_usuario=?";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try{
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, ""+c.getId());
+            rs = stmt.executeQuery();
+            if(rs.next()){
+                String email = rs.getString("email");
+                c.setEmail(email);
+                String senha = rs.getString("senha");
+                c.setSenha(senha);
+            } else{
+                JOptionPane.showMessageDialog(null, "Houve algum erro");
+                c.setEmail("");
+                c.setSenha("");
+            }
+            
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Houve algum" + e.toString());
+        } finally{
+            DBConexion.closeConnection(con, stmt, rs);
+        }
+            
+     }
+    
+    public boolean verificaEmail(Cliente c) throws SQLException{
+        Connection con = DBConexion.getConnection();
+        String sql = "Select email from usuarios where email=?";
+        boolean existe = false;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        try{
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, c.getEmail());
+            rs = stmt.executeQuery();
+            
+            if(rs.next()){
+                existe = true;
+            } else {
+                existe = false;
+            }
+            
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Houve algum erro " + e.toString());
+        } finally {
+            DBConexion.closeConnection(con, stmt, rs);
+        }
+        
+        return existe;
+    }
+    
+    public void alterarRegistro(Cliente c) throws SQLException {
+        Connection con = DBConexion.getConnection();
+        PreparedStatement stmt = null;
+        String sql1 = "update usuarios set senha=? where id_usuario=?";
+        String sql2 = "update usuarios set email=? where id_usuario=?";
+        String sql3 = "update usuarios set email=?, senha=? where id_usuario=?";
+        try{
+            if(c.getEmail().equals("")){
+                stmt = con.prepareStatement(sql1);
+                stmt.setString(1, c.getSenha());
+                stmt.setString(2, ""+ c.getId());
+                
+                stmt.executeUpdate();
+                
+                
+            } else if (c.getSenha().equals("")){
+                stmt = con.prepareStatement(sql2);
+                stmt.setString(1, c.getEmail());
+                stmt.setString(2, "" + c.getId());
+                
+                stmt.executeUpdate();
+                
+            }else{
+                stmt = con.prepareStatement(sql3);
+                stmt.setString(1, c.getEmail());
+                stmt.setString(2, c.getSenha());
+                stmt.setString(3, "" + c.getId());
+                
+                stmt.executeUpdate();
+                
+                
+            }
+            
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Houve algum erro ao atualizar seus dados" + e.toString());
+        }finally{
+            DBConexion.closeConnection(con, stmt);
         }
     }
     
+    public void deletarDados2(Cliente c) throws SQLException{
+        Connection con = DBConexion.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "delete from usuarios where id_usuario = ?";
+        try{
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, "" + c.getId());        
+            stmt.executeUpdate();
+            c.setId(0);
+                        
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Houve algum erro ao deletar seus dados" + e.toString());
+        }finally{
+            DBConexion.closeConnection(con, stmt);
+        }
+    }
+    
+    public void deletarDados1(Cliente c) throws SQLException{
+        Connection con = DBConexion.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "delete from respostas where id_usuario = ?";
+        
+        try{
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, "" + c.getId());
+            
+            stmt.executeUpdate();
+            
+            deletarDados2(c);
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Houve algum erro ao deletar seus dados" + e.toString());
+        }finally{
+            DBConexion.closeConnection(con, stmt);
+        }
+    }
+    
+    public void deletarDados3(Cliente c) throws SQLException{
+        Connection con = DBConexion.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "delete from respostas where id_usuario = ?";
+        
+        try{
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, "" + c.getId());
+            
+            stmt.executeUpdate();
+            
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Houve algum erro ao deletar seus dados" + e.toString());
+        }finally{
+            DBConexion.closeConnection(con, stmt);
+        }
+    }
 }
